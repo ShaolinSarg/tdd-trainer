1b2edfe5914f00a6134542df9f56181d564eaaca
David Sarginson <David.Sarginson@OpencastSoftware.com>
2016-06-23 17:06:29 +0100

    adding project path

--- src/clj/tdd_trainer/routes/snapshot.clj
+++ src/clj/tdd_trainer/routes/snapshot.clj
@@ -10,9 +10,10 @@
 
 (defn format-session-data
   "formats the session data for json serialisation"
-  [{:keys [session-id start-time snapshots]}]
+  [{:keys [session-id start-time project-root snapshots]}]
   {:sessionId session-id
    :startTime (f/unparse json-date-formatter start-time)
+   :projectBase project-root
    :snapshots (map (fn [item] {
                                :timestamp (f/unparse json-date-formatter (:timestamp item))
                                :failingTestCount (:failing-test-count item)
@@ -21,8 +22,8 @@
    })
 
 (defroutes snapshot-routes 
-  (POST "/session" [timestamp]
-        (let [new-session (create-session timestamp)]
+  (POST "/session" [timestamp projectBase]
+        (let [new-session (create-session timestamp projectBase)]
           (reset! d/session-data new-session)
           (response/created (format-session-data new-session))))
   
@@ -30,7 +31,8 @@
        (response/ok {:sessions [(str "http://localhost:3000/session/" (:session-id @d/session-data))]}))
 
 
-  (GET "/session/:session-id" [session-id] (response/ok (format-session-data @d/session-data)))
+  (GET "/session/:session-id" [session-id]
+       (response/ok (format-session-data @d/session-data)))
 
     
   (POST "/session/:session-id/snapshot" [session-id timestamp failingTestCount failingTestNames]
@@ -38,10 +40,10 @@
           (d/update-session-data session-id {:snapshot-timestamp (f/parse json-date-formatter timestamp)
                                              :failing-test-count failingTestCount
                                              :failing-test-names failingTestNames})
-          (response/ok))))
+          (response/ok)))
 
 
   (GET "/session/:session-id/stats" [session-id]
        (let [stats (gen-stat-summary @d/session-data)]
-         (response/ok stats)))
+         (response/ok stats))))
 
